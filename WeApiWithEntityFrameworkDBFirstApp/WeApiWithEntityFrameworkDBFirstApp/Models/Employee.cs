c88edda9640ba41a2348ac9f80bc9cf63faab78f
unknown <cogdotnet1691@iiht.tech>
2022-08-28 17:10:12 +0530

    Web API & Entity Framework With DB First Approch

---
+++ WeApiWithEntityFrameworkDBFirstApp/WeApiWithEntityFrameworkDBFirstApp/Models/Employee.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace WeApiWithEntityFrameworkDBFirstApp.Models
+{
+    public partial class Employee
+    {
+        public int EmpId { get; set; }
+        public string EmpName { get; set; } = null!;
+        public decimal Salary { get; set; }
+        public int? RolId { get; set; }
+
+        public virtual Role? Rol { get; set; }
+    }
+}
---
+++ WeApiWithEntityFrameworkDBFirstApp/WeApiWithEntityFrameworkDBFirstApp/Models/EntityFrameworkLearningContext.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata;
+
+namespace WeApiWithEntityFrameworkDBFirstApp.Models
+{
+    public partial class EntityFrameworkLearningContext : DbContext
+    {
+        public EntityFrameworkLearningContext()
+        {
+        }
+
+        public EntityFrameworkLearningContext(DbContextOptions<EntityFrameworkLearningContext> options)
+            : base(options)
+        {
+        }
+
+        public virtual DbSet<Employee> Employees { get; set; } = null!;
+        public virtual DbSet<Role> Roles { get; set; } = null!;
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured)
+            {
+#warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see http://go.microsoft.com/fwlink/?LinkId=723263.
+                optionsBuilder.UseSqlServer("Server=(localdb)\\mssqllocaldb; Database=EntityFrameworkLearning;Trusted_Connection=True;");
+            }
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Employee>(entity =>
+            {
+                entity.HasKey(e => e.EmpId);
+
+                entity.ToTable("Employee");
+
+                entity.Property(e => e.EmpId)
+                    .ValueGeneratedNever()
+                    .HasColumnName("EmpID");
+
+                entity.Property(e => e.EmpName).HasMaxLength(100);
+
+                entity.Property(e => e.RolId).HasColumnName("RolID");
+
+                entity.Property(e => e.Salary).HasColumnType("decimal(18, 2)");
+
+                entity.HasOne(d => d.Rol)
+                    .WithMany(p => p.Employees)
+                    .HasForeignKey(d => d.RolId)
+                    .HasConstraintName("FK_Employee_Roles");
+            });
+
+            modelBuilder.Entity<Role>(entity =>
+            {
+                entity.Property(e => e.RoleId)
+                    .ValueGeneratedNever()
+                    .HasColumnName("RoleID");
+
+                entity.Property(e => e.Name).HasMaxLength(50);
+            });
+
+            OnModelCreatingPartial(modelBuilder);
+        }
+
+        partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+    }
+}
